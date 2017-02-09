0febe59398587f2528c283c2a074215b61a15d4d
Roman Sill <RuntimeEx@gmail.com>
2017-02-08 22:57:31 -0500

    walking through tutorial

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,6 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+
+
+var awesomeThoughts = "I am Hitler, and I am awesome!";
+var funThoughts = awesomeThoughts.replace("awesome", "fun");
+console.log(awesomeThoughts);
+$("#main").append(funThoughts);
